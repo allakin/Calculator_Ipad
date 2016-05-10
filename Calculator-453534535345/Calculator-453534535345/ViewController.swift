8aa10bd211a9b99a8eba465d9527812fa65910b3
Pavel <19paul91@mail.ru>
2016-05-10 22:21:32 +0300

    default setting

---
+++ Calculator-453534535345/Calculator-453534535345/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator-453534535345
+//
+//  Created by Павел Анплеенко on 10/05/16.
+//  Copyright © 2016 Pavel Anpleenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
