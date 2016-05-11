fcb4c0cd3a0bbf8f354118107dd0238812310ae3
Pavel <19paul91@mail.ru>
2016-05-11 21:46:36 +0300

    4 complete

--- Calculator-453534535345/Calculator-453534535345/ViewController.swift
+++ Calculator-453534535345/Calculator-453534535345/ViewController.swift
@@ -10,19 +10,21 @@
 // 1. Добавить Label (+)
 // 2. Добавить iuview (+)
 // 3. Добавить цифры (+)
-// 4. Написать код чтобы выводилился текст на экране по нажатию на кнопку
+// 4. Написать код чтобы выводилился текст на экране по нажатию на цифры (+)
+// 5. Отрезать трое точие когда цифр много
 
 import UIKit
 
 class ViewController: UIViewController {
-
-  @IBOutlet weak var dysplayLabel: UILabel!
   
+  @IBOutlet weak var displayResultLabel: UILabel!
   @IBAction func numberButton(sender: UIButton) {
-   
-    let number
+    
+    let number = sender.currentTitle! //получает значение при нажатии на кнопку
+    
+    displayResultLabel.text = displayResultLabel.text! + number // Мы хотим чтобы displayResultLabel отобразил свой текст + вывел значение полученные из цифр
     
   }
-
+  
 }
 
